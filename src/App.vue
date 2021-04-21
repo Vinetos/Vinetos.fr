4f8cc925eb42e3973a219f7c94cab13533eb12dd
Vinetos <Vinetosdev@gmail.com>
2021-04-21 19:19:38 +0200

    feat: Rework website

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,41 @@
 </template>
 
 <script>
-
 import Main from "./components/Main";
 
 export default {
   name: 'App',
   components: {
-    Main
+    Main,
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+@font-face {
+  font-family: "Sans Forgetica";
+  src: url(assets/SansForgetica-Regular.otf);
+}
+
+// Import Bulma's core
+@import "~bulma/sass/utilities/_all";
+
+$family-code: "Sans Forgetica";
+
+@import "~bulma";
+@import "~buefy/src/scss/buefy";
+
+
+* {
+  overflow: hidden !important;
+}
+
+/* Particles JS */
+.particles-js-canvas-el {
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  z-index: -1;
+  background-image: linear-gradient(141deg, #090808 0%, #222 71%, #312d2c 100%);
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import { library, dom } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import App from './App.vue'
-import 'buefy/dist/buefy.css';
 
 Vue.config.productionTip = false
 
